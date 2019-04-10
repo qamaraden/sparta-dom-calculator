5782ae8a58d1028d83407798f7bb69517e17c057
Tech-A29-clean <tech-a29-clean@Tech-A29s-MacBook-Air.local>
2019-04-10 20:34:41 +0100

    added changes

---
+++ dom-calculator-starter-code/js/calc.js
@@ -0,0 +1,25 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+
+  var buttons = document.querySelectorAll("buttonNum")
+  for (var i = 0; i < buttons.length; ){
+    var buttons = buttons[i];
+
+    button.addEventListener("click", function(event){
+      console.log(this.value + "was clicked"); // this always refers to the button that was clicked
+    })
+  }
+ for (var i = 0; i < buttons.length; i++) {
+   buttons[i]
+ }
+
+
+
+})
+
+
+//log this
+//add the string oof the button
+//sum = ""
+//sum += this.innerHTML
+//display.this.innerHTML
